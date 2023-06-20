3ec62f3bc554e7bce2339e58c4a843c28cec669b
Chell <devchell@outlook.com>
2023-06-20 20:17:36 -0300

    Using try/catch

--- Screen_Match_v2/src/br/com/alura/screenmatch/main/MainWithSearch.java
+++ Screen_Match_v2/src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -21,30 +21,38 @@ public class MainWithSearch {
 
         var search = scanner.nextLine();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=" + search + "&apikey=eeac376e"))
-                .build();
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://www.omdbapi.com/?t=" + search + "&apikey=eeac376e"))
+                    .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        //System.out.println(response.body());
+            //System.out.println(response.body());
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
 
-        //System.out.println("\nTitle: " + myTitle.getName());
-        //System.out.println("Year Released: " + myTitle.getYearReleased());
-        //System.out.println("\n" + myTitle);
+            //System.ou]t.println("\nTitle: " + myTitle.getName());
+            //System.out.println("Year Released: " + myTitle.getYearReleased());
+            //System.out.println("\n" + myTitle);
 
-        TitleOMBd myTitleOMBd = gson.fromJson(response.body(), TitleOMBd.class);
-        System.out.println(myTitleOMBd);
+            TitleOMBd myTitleOMBd = gson.fromJson(response.body(), TitleOMBd.class);
+            System.out.println(myTitleOMBd);
 
-        Titles myTitle = new Titles(myTitleOMBd);
-        System.out.println(myTitle);
-
-
+            //try {
+            Titles myTitle = new Titles(myTitleOMBd);
+            System.out.println(myTitle);
+        } catch (NumberFormatException e) {
+            System.out.print("ERROR! ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e){
+            System.out.println("Invalid URL! Try again");
+        } finally {
+            System.out.println("Finalizing!");
+        }
     }
 }
